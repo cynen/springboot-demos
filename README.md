76d7530728a2fd38a57fdbe4ec24cb9e4781a719
caineng <cai.neng@zte.com.cn>
2020-06-02 09:35:26 +0800

    添加一些小demo.

--- README.md
+++ README.md
@@ -7,4 +7,51 @@ springboot  demos
 此处为了区别,使用了mysql和mssql2种数据库. 
 
 
+一些小demo
+
+# WordTest项目:
+项目需求是使用提供的wrod文档,在需要的地方填充对应的数据,生成新的wrod文档.
+--> 也就是按照wrod模板,填充数据,生成新的word文档.
+-->目前直接仅仅 使用freemarker
+
+
+
+
+# FileUpload
+主要是学习upload组件进行文件上传的功能实现.
+<groupId>commons-io</groupId>
+<artifactId>commons-io</artifactId>
+
+<groupId>commons-fileupload</groupId>
+<artifactId>commons-fileupload</artifactId>
+
+
+# DataStruceStudy 
+主要是用于学习java实现的数据结构!
+算法等.
+1.最大堆
+2.最小堆
+
+
+排序:
+1.基数排序--俗称桶排序.   
+
+
+
+FileUpload 
+文件上传的实例
+
+MyTomcat:
+手写一个简单版的Tomcat.
+
+RedisStudy:
+学习Redis的记录.各种操作,知识点等.
+
+SSM-CRM:
+一个整合SpringMVC + Spring + Mybaties的简单项目
+实现基本的CRUD
+
+
+
+
 
---
+++ WordTest/WordTest/src/main/java/com/myth/WordTest/WordUtils.java
@@ -0,0 +1,84 @@
+package com.myth.WordTest;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.Map;
+
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+/**
+ * 
+* <p>Title: WordUtils</p>  
+* <p>Description: 
+*  仅仅使用freemarker实现
+* wrod模板生成wrod文档.
+* </p>  
+* @author myth
+* @date 2018年9月10日
+ */
+public class WordUtils {
+    //配置信息,代码本身写的还是很可读的,就不过多注解了
+    private static Configuration configuration = null;
+    //这里注意的是利用WordUtils的类加载器动态获得模板文件的位置
+    // 实际加载的src目录下的.
+    private static final String templateFolder =  WordUtils.class.getClassLoader().getResource("./").getPath();
+    static {
+    	// 设置版本.
+    	configuration = new Configuration(Configuration.VERSION_2_3_23);
+    	configuration.setDefaultEncoding("utf-8");
+        try {
+        	// 设置模板路径文件夹.
+            configuration.setDirectoryForTemplateLoading(new File(templateFolder));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+   }
+    /**
+     * 
+     * <p>Description: </p>  
+     * @param map
+     * @throws Exception
+     */
+    public static void creatWord(Map map) throws Exception {
+    	// 具体的模板对象.
+        Template freemarkerTemplate = configuration.getTemplate("wps.ftl");
+        File file = null;
+        try {
+            // 调用工具类的createDoc方法生成Word文档
+            file = createDoc(map,freemarkerTemplate);
+        } finally {
+           //  if(file != null) file.delete(); // 删除临时文件
+        }
+    }
+ 
+    /**
+     * 生成word 数据.
+     * 
+     * @param dataMap
+     * @param template
+     * @return
+     * @throws Exception
+     */
+    private static File createDoc(Map<?, ?> dataMap, Template template) throws Exception {
+        String name =  "F:/wps.doc";
+        File f = new File(name);
+        if(!f.exists()) {
+        	f.createNewFile();
+        }
+        Template t = template;
+        try {
+            // 这个地方不能使用FileWriter因为需要指定编码类型否则生成的Word文档会因为有无法识别的编码而无法打开
+            Writer w = new OutputStreamWriter(new FileOutputStream(f), "utf-8");
+            t.process(dataMap, w);
+            w.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            throw new RuntimeException(ex);
+        }
+        return f;
+    }
+}
